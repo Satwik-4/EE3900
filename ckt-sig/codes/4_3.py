176a393d725acb1ee2e58f2b40336287c4056070
“Satwik <ep20btech11027@iith.ac.in>
2022-11-09 15:55:54 +0530

    Modified

--- ckt-sig/codes/4_3.py
+++ ckt-sig/codes/4_3.py
@@ -1,13 +1,16 @@
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+import os
 
-s = np.linspace(0,10e6)
-H = 0.5e6/(s+1.5e6)
-plt.figure()
+N = 100000
+s = np.linspace(0, 10000000, N)
+R1 = 1
+R2 = 2
+C = 1e-6
+H = R1/(R1+R2+s*C*R1*R2)
 plt.plot(s,H)
 plt.grid()
-plt.xlabel('$s$')
-plt.ylabel('$H(s)$')
-plt.title('Transfer function')
-# plt.show()
-plt.savefig('../figs/transfer.png')
+plt.xlabel('s')
+plt.ylabel('H(s)')
+plt.savefig('../figs/4_3.png')
+os.system('sh gopen.sh ../figs/4_3.png')
